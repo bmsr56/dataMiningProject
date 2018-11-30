40d0724d9beed37571d7a00e0233b3ae1a001b94
Nathan <nawwx3@mst.edu>
2018-11-29 22:44:44 -0600

    cleaned code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cleaningTools
 import pandas as pd
 import numpy as np
-import string
 
 df_small = pd.read_csv('lyrics_small.csv')
 
@@ -9,9 +8,7 @@ print(df_small)
 print(list(df_small.columns.values))
 
 for index, row in df_small.iterrows():
-    print(row['song'], row['genre'])
-    print(type(row['lyrics']))
-    stringA = row['lyrics'].replace('\n', ' ')
-    print(type(stringA))
+    lyrics = row['lyrics'].replace('\n', ' ')
+    cleaned_lyrics_list = cleaningTools.normalize(lyrics)
 
-    print(cleaningTools.normalize(stringA))
+    # TODO: assign lyrics list back into dataframe for later use
