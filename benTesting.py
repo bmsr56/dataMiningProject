c1319ec6ce7e05df4adf7048c121c3ac651f0f00
Ben Simpson <bmsr56@mst.edu>
2018-12-02 17:33:12 -0600

    Adding some data cleaning tools in benTesting

--- benTesting.py
+++ benTesting.py
@@ -1,6 +1,29 @@
+# index, song, year, artist, genre, lyrics
+
 import pandas as pd
 import numpy as np
 
-df_small = pd.read_csv('lyrics_small.csv')
+def header(desc):
+    d = '[ {} ]'.format(desc)
+    print('-'*len(d))
+    print(d)
+    print('-'*len(d))
 
-print(df_small)
+df = pd.read_csv('lyrics_small.csv')
+
+#replace carriage returns
+header('replaced carriage returns')
+df = df.replace({'\n': ' '}, regex=True)
+
+#count the words in each song
+df['word_count'] = df['lyrics'].str.split().str.len()
+
+#let's see what the songs with 1 word look like
+df1 = df.loc[df['word_count'] == 1]
+
+#elimintate the 1-word songs and review the data again
+df = df[df['word_count'] != 1]
+df['word_count'].groupby(df['genre']).describe()
+
+
+print(df)
